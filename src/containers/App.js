037558fcb07a35e4698ed315f7b70a8ed4b4dc07
Yishuen Lo <lo.yishuen@gmail.com>
2020-07-08 18:53:24 -0600

    update dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import CharacterGenerator from "./scenes/inputOutput/CharacterGenerator";
-import StarterPokemon from "./scenes/StarterPokemon/StarterPokemon";
+import CharacterGenerator from "../scenes/inputOutput/CharacterGenerator";
+import StarterPokemon from "../scenes/StarterPokemon/StarterPokemon";
 
 class App extends Component {
   render() {
